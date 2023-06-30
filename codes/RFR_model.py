a8acaf3896cfe4f5a57c35096cd9cd72ca7fe987
Steven <steven.chu.csu@gmail.com>
2023-06-30 14:13:34 +0800

    rename RF to RFR

--- codes/RFR_model.py
+++ codes/RFR_model.py
@@ -3,17 +3,17 @@ from sklearn.ensemble import RandomForestRegressor as RFR
 
 
 def model(features):
-    RF_model = RFR(n_estimators=60, oob_score=True, max_depth=6, random_state=0)
-    RF_model.fit(x_train, y_train)
-    y_train_pre = RF_model.predict(x_train)
-    y_test_pre = RF_model.predict(x_test)
+    RFR_model = RFR(n_estimators=60, oob_score=True, max_depth=6, random_state=0)
+    RFR_model.fit(x_train, y_train)
+    y_train_pre = RFR_model.predict(x_train)
+    y_test_pre = RFR_model.predict(x_test)
     utils.plot_scatter(y_train, y_train_pre, y_test, y_test_pre)
     rmse, r2, rmse_t, r2_t = utils.metrics(y_train, y_train_pre, y_test, y_test_pre)
     print(f"RMSE is {rmse:.3f}, R2 is {r2:.3f}, RMSE on test is {rmse_t:.3f}, R2 on test is {r2_t:.3f}")
-    utils.plot_feature_importance(RF_model.feature_importances_, features, "RF")
-    print("Predicted results: {}".format(list(RF_model.predict(x_test))))
+    utils.plot_feature_importance(RFR_model.feature_importances_, features, "RFR")
+    print("Predicted results: {}".format(list(RFR_model.predict(x_test))))
     print("DFT results: {}".format(list(y_test)))
-    return RF_model
+    return RFR_model
 
 
 if __name__ == "__main__":
@@ -26,4 +26,4 @@ if __name__ == "__main__":
     samples["Difference from optimal G"] = abs(samples["Predicted value"] - OPTIMAL_G)
     potential_candidates = samples.sort_values(by=["Difference from optimal G"], ascending=True, inplace=False)
     print(potential_candidates.head(20))
-    samples.to_csv("../data/predicted_result_RF.csv", index=False)
+    samples.to_csv("../data/predicted_result_RFR.csv", index=False)
